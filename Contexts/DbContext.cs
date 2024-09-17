d295793806294b4bd7f6b114a0395aa488e2c951
Gustavo Lima Carvalho <gustavocrvlh.02@gmail.com>
2024-09-17 19:47:48 -0300

    removed comments

--- Contexts/DbContext.cs
+++ Contexts/DbContext.cs
@@ -9,7 +9,7 @@ public class LibraryContext : DbContext
         var folderPath = Path.Combine(Environment.CurrentDirectory, "Database");
         if (!Directory.Exists(folderPath))
         {
-            Directory.CreateDirectory(folderPath); // Cria a pasta se ela não existir
+            Directory.CreateDirectory(folderPath);
         }
         var dbPath = Path.Combine(folderPath, "library.db");
         optionsBuilder.UseSqlite($"Data Source={dbPath}");
